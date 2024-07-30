875730d1d898066f13ebe1e0508128f9237578ce
Leachim <32847549+Licheam@users.noreply.github.com>
2024-07-31 00:51:06 +0800

    impl genetic BIT, tmp fix some scanln bug (#3)
    * tmp fix to scanln macro
    
    * fix use core
    
    * impl genetic BIT
    
    * fmt
    
    * update version
    
    * add cargo deny config

--- src/data_structure/binary_indexed_tree.rs
+++ src/data_structure/binary_indexed_tree.rs
@@ -1,13 +1,21 @@
+use crate::num::{Numeric, NumericAssOps};
+
 #[derive(Debug)]
-pub struct BinaryIndexedTree {
-    b: Vec<isize>,
+pub struct BinaryIndexedTree<N>
+where
+    N: Numeric + NumericAssOps,
+{
+    b: Vec<N>,
     n: usize,
 }
 
-impl BinaryIndexedTree {
+impl<N> BinaryIndexedTree<N>
+where
+    N: Numeric + NumericAssOps,
+{
     pub fn new(n: usize) -> Self {
         Self {
-            b: vec![0; n + 1],
+            b: vec![N::ZERO; n + 1],
             n,
         }
     }
@@ -16,33 +24,31 @@ impl BinaryIndexedTree {
         x & (!x + 1)
     }
 
-    pub fn change(&mut self, mut x: usize, y: isize) {
-        let BinaryIndexedTree { b, n } = self;
-        while x <= *n {
-            b[x] += y;
+    pub fn change(&mut self, mut x: usize, y: N) {
+        while x <= self.n {
+            self.b[x] += y;
             x += Self::lowbit(x);
         }
     }
 
-    pub fn sum(&self, mut x: usize) -> isize {
-        let BinaryIndexedTree { b, .. } = &self;
-        let mut s: isize = 0;
+    pub fn sum(&self, mut x: usize) -> N {
+        let mut s = N::ZERO;
         while x > 0 {
-            s += b[x];
+            s += self.b[x];
             x -= Self::lowbit(x);
         }
         s
     }
 
-    pub fn build(&mut self, a: &Vec<isize>) {
-        let BinaryIndexedTree { b, n } = self;
-        for i in 1..=*n {
-            b[i] = a[i - 1];
+    pub fn build(&mut self, a: &Vec<N>) {
+        for i in 1..=self.n {
+            self.b[i] = a[i - 1];
         }
         let mut x = 1;
-        while x << 1 <= *n {
-            for i in (x..=*n - x).step_by(x << 1) {
-                b[i + x] += b[i];
+        while x << 1 <= self.n {
+            for i in (x..=self.n - x).step_by(x << 1) {
+                let t = self.b[i];
+                self.b[i + x] += t;
             }
             x <<= 1;
         }
