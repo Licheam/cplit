d3f6870f298326ea5afa0ca6ad4c5fe715e36d3e
Leachim <32847549+Licheam@users.noreply.github.com>
2024-07-30 23:51:08 +0800

    tmp fix to scanln macro

--- src/io/macros.rs
+++ src/io/macros.rs
@@ -48,14 +48,40 @@ macro_rules! fscanln {
 #[macro_export]
 macro_rules! scanln {
     ($($i:expr), +) => {
-        $crate::fscanln!(std::io::stdin(), $($i), +);
+        let mut iter = std::iter::repeat_with(|| {
+            let mut buf = String::new();
+            std::io::stdin().read_line(&mut buf).unwrap();
+            buf.split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .flatten();
+        $(
+            $i = iter.next().unwrap();
+        )*
     };
 
     ($coll:expr ; $n:expr) => {
-        $crate::fscanln!(std::io::stdin(), $coll ; $n);
+        $coll = std::iter::once(Default::default())
+                .chain(
+                    std::iter::repeat_with(|| {
+                        let mut buf = String::new();
+                        std::io::stdin().read_line(&mut buf).unwrap();
+                        buf.split_whitespace()
+                            .map(|x| x.parse().unwrap())
+                            .collect::<Vec<_>>()
+                    })
+                    .flatten()
+                    .take($n),
+                )
+                .collect();
     };
 
     ($coll:expr ;) => {
-        $crate::fscanln!(std::io::stdin(), $coll ;);
+        let mut buf = String::new();
+        std::io::stdin().read_line(&mut buf).unwrap();
+        $coll = std::iter::once(Default::default())
+                .chain(buf.split_whitespace().map(|x| x.parse().unwrap()))
+                .collect();
     };
 }
