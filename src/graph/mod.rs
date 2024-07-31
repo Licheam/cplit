05fe8b09c9df0b8f176dbe2ff4db2f07ff51ae71
Leachim <32847549+Licheam@users.noreply.github.com>
2024-07-31 12:58:06 +0800

    update doc

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -15,12 +15,13 @@
 //!     let (n, m, s): (usize, usize, usize);
 //!     // Read the number of nodes, the number of edges, and the source node.
 //!     scanln!(n, m, s);
-//!     // Create a graph with n nodes and storing nothing in each node, and a usize in each edge as weight.
+//!     // Create a graph with n nodes and storing nothing in each node,
+//!     // and a usize in each edge as weight.
 //!     let mut graph = Graph::<(), usize>::new(n);
 //!     for _ in 0..m {
-//!         // Read an edge u->v with weight w.
 //!         let (u, v, w): (usize, usize, usize);
 //!         scanln!(u, v, w);
+//!         // Create an edge u->v with weight w.
 //!         graph.add_edge(u, v, w);
 //!     }
 //!     let dist = dijkstra(s, &graph);
