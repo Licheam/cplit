cea4d2e9fdffde7f275f5514b951cf0a29a8e56f
Leachim <32847549+Licheam@users.noreply.github.com>
2024-07-31 10:59:04 +0800

    try fix calling macros multiple times

--- src/io/macros.rs
+++ src/io/macros.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! fscanln {
-    ($reader:expr, $($i:expr), +) => {
+    ($reader:expr, $($i:expr), +) => {{
         #[allow(unused_imports)]
         use std::io::BufRead;
         let mut iter = std::iter::repeat_with(|| {
@@ -14,9 +14,9 @@ macro_rules! fscanln {
         $(
             $i = iter.next().unwrap();
         )*
-    };
+    }};
 
-    ($reader:expr, $coll:expr ; $n:expr) => {
+    ($reader:expr, $coll:expr ; $n:expr) => {{
         #[allow(unused_imports)]
         use std::io::BufRead;
         $coll = std::iter::once(Default::default())
@@ -32,9 +32,9 @@ macro_rules! fscanln {
                     .take($n),
                 )
                 .collect();
-    };
+    }};
 
-    ($reader:expr, $coll:expr ;) => {
+    ($reader:expr, $coll:expr ;) => {{
         #[allow(unused_imports)]
         use std::io::BufRead;
         let mut buf = String::new();
@@ -42,46 +42,20 @@ macro_rules! fscanln {
         $coll = std::iter::once(Default::default())
                 .chain(buf.split_whitespace().map(|x| x.parse().unwrap()))
                 .collect();
-    };
+    }};
 }
 
 #[macro_export]
 macro_rules! scanln {
     ($($i:expr), +) => {
-        let mut iter = std::iter::repeat_with(|| {
-            let mut buf = String::new();
-            std::io::stdin().read_line(&mut buf).unwrap();
-            buf.split_whitespace()
-                .map(|x| x.parse().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .flatten();
-        $(
-            $i = iter.next().unwrap();
-        )*
+        $crate::fscanln!(std::io::stdin(), $($i), +);
     };
 
     ($coll:expr ; $n:expr) => {
-        $coll = std::iter::once(Default::default())
-                .chain(
-                    std::iter::repeat_with(|| {
-                        let mut buf = String::new();
-                        std::io::stdin().read_line(&mut buf).unwrap();
-                        buf.split_whitespace()
-                            .map(|x| x.parse().unwrap())
-                            .collect::<Vec<_>>()
-                    })
-                    .flatten()
-                    .take($n),
-                )
-                .collect();
+        $crate::fscanln!(std::io::stdin(), $($i), +);
     };
 
     ($coll:expr ;) => {
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).unwrap();
-        $coll = std::iter::once(Default::default())
-                .chain(buf.split_whitespace().map(|x| x.parse().unwrap()))
-                .collect();
+        $crate::fscanln!(std::io::stdin(), $($i), +);
     };
 }
