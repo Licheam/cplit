80329a2d1f5cdc761704d9e9f8464f53cf36983f
Leachim <32847549+Licheam@users.noreply.github.com>
2024-08-01 23:28:50 +0800

    try impl a better segment tree

---
+++ src/data_structure/segment_tree/ops.rs
@@ -0,0 +1,77 @@
+use std::marker::PhantomData;
+use std::fmt::Debug;
+use crate::num::{Numeric, NumericAssOps, NumericOps};
+
+pub trait Operation<N>
+where
+    N: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    <N as TryFrom<usize>>::Error: Debug,
+{
+    /// Combines two values from left and right child.
+    const COMBINE: fn(left_child: N, right_child: N) -> N;
+
+    /// Pushes the tag down to the left or right child.
+    const PUSH_BUF: fn(child: N, tag: N, len: usize) -> N;
+    const PUSH_TAG: fn(child_tag: N, tag: N) -> N;
+}
+
+pub struct Sum;
+
+impl<N> Operation<N> for Sum
+where
+    N: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    <N as TryFrom<usize>>::Error: Debug,
+{
+    const COMBINE: fn(N, N) -> N = |left_child, right_child| left_child + right_child;
+    const PUSH_BUF: fn(N, N, usize) -> N = |child, tag, len| child + tag * N::try_from(len).unwrap();
+    const PUSH_TAG: fn(N, N) -> N = |child_tag, tag| child_tag + tag;
+}
+
+pub struct OperationPair<N1, N2, O1, O2>
+where
+    N1: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    N2: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    <N1 as TryFrom<usize>>::Error: Debug,
+    <N2 as TryFrom<usize>>::Error: Debug,
+    O1: Operation<N1>,
+    O2: Operation<N2>,
+{
+    phantoms: (
+        PhantomData<N1>,
+        PhantomData<N2>,
+        PhantomData<O1>,
+        PhantomData<O2>,
+    ),
+}
+
+impl<N1, N2, O1, O2> Operation<(N1, N2)> for OperationPair<N1, N2, O1, O2>
+where
+    N1: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    N2: Numeric + NumericOps + NumericAssOps + Clone + Copy + TryFrom<usize>,
+    <N1 as TryFrom<usize>>::Error: Debug,
+    <N2 as TryFrom<usize>>::Error: Debug,
+    O1: Operation<N1>,
+    O2: Operation<N2>,
+{
+    const COMBINE: fn((N1, N2), (N1, N2)) -> (N1, N2) =
+        |(left_child1, left_child2), (right_child1, right_child2)| {
+            (
+                O1::COMBINE(left_child1, right_child1),
+                O2::COMBINE(left_child2, right_child2),
+            )
+        };
+    const PUSH_BUF: fn((N1, N2), (N1, N2), usize) -> (N1, N2) =
+        |(child1, child2), (tag1, tag2), len| {
+            (
+                O1::PUSH_BUF(child1, tag1, len),
+                O2::PUSH_BUF(child2, tag2, len),
+            )
+        };
+    const PUSH_TAG: fn((N1, N2), (N1, N2)) -> (N1, N2) =
+        |(child_tag1, child_tag2), (tag1, tag2)| {
+            (
+                O1::PUSH_TAG(child_tag1, tag1),
+                O2::PUSH_TAG(child_tag2, tag2),
+            )
+        };
+}
