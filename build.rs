e581c84ad05ec8ce455be0023e2dd5072d59cb8b
Leachim <32847549+Licheam@users.noreply.github.com>
2024-08-18 22:14:56 +0800

    gen

--- build.rs
+++ build.rs
@@ -1,20 +1,10 @@
 use rust_bundler::Bundler;
 fn main() {
-    let mut bundler = Bundler::new(
-        "cplit",
-        "singlefile.rs.example",
-        "singlefile.rs",
-        true,
-    );
+    let mut bundler = Bundler::new("cplit", "singlefile.rs.example", "singlefile.rs", true);
 
     bundler.run();
-    
-    let mut bundler = Bundler::new(
-        "cplit",
-        "singlefile.rs.example",
-        "multiline.rs",
-        false,
-    );
+
+    let mut bundler = Bundler::new("cplit", "singlefile.rs.example", "multiline.rs", false);
 
     bundler.run();
 }
--- multiline.rs
+++ multiline.rs
@@ -406,39 +406,59 @@ pub mod cplit {
 	}
 	pub mod general {
 		
-		pub mod binary_search {
-			use crate::cplit::num::{Bounded, Numeric, NumericCmpOps, NumericOps};
-			use std::ops::{Bound, RangeBounds};
-			
-			pub fn binary_search<N>(bounds: impl RangeBounds<N>, f: impl Fn(N) -> bool) -> N
-			where
-			    N: Numeric + NumericOps + NumericCmpOps + Copy + Clone + Bounded,
-			{
-			    let mut left = match bounds.start_bound() {
-			        Bound::Included(&s) => s,
-			        Bound::Excluded(&s) => s + N::ONE,
-			        Bound::Unbounded => N::MIN,
-			    };
-			    let mut right = match bounds.end_bound() {
-			        Bound::Included(&e) => e + N::ONE,
-			        Bound::Excluded(&e) => e,
-			        Bound::Unbounded => N::MAX,
-			    };
-			
-			    while left < right {
-			        let mid = (left + right) / (N::ONE + N::ONE);
-			        if f(mid) {
-			            right = mid;
-			        } else {
-			            left = mid + N::ONE;
-			        }
-			    }
-			
-			    left
-			}
+		use crate::cplit::num::{Bounded, Numeric, NumericCmpOps, NumericOps};
+		use std::cmp::{Ord, Ordering};
+		use std::ops::{Bound, RangeBounds};
+		
+		pub fn binary_search<N>(bounds: impl RangeBounds<N>, f: impl Fn(N) -> bool) -> N
+		where
+		    N: Numeric + NumericOps + NumericCmpOps + Copy + Clone + Bounded,
+		{
+		    let mut left = match bounds.start_bound() {
+		        Bound::Included(&s) => s,
+		        Bound::Excluded(&s) => s + N::ONE,
+		        Bound::Unbounded => N::MIN,
+		    };
+		    let mut right = match bounds.end_bound() {
+		        Bound::Included(&e) => e + N::ONE,
+		        Bound::Excluded(&e) => e,
+		        Bound::Unbounded => N::MAX,
+		    };
+		
+		    while left < right {
+		        let mid = (left + right) / (N::ONE + N::ONE);
+		        if f(mid) {
+		            right = mid;
+		        } else {
+		            left = mid + N::ONE;
+		        }
+		    }
+		
+		    left
 		}
-		#[doc(inline)]
-		pub use self::binary_search::binary_search;
+		
+		pub fn next_permutation<T>(arr: &mut [T]) -> bool
+		where
+		    T: Ord,
+		{
+		    if arr.len() == 0 {
+		        return false;
+		    }
+		
+		    if let Some(i) = (0..arr.len() - 1).rev().find(|&i| arr[i] < arr[i + 1]) {
+		        (i + 1..arr.len())
+		            .rev()
+		            .find(|&j| arr[i] < arr[j])
+		            .map(|j| {
+		                arr.swap(i, j);
+		            });
+		        arr[i + 1..].reverse();
+		        true
+		    } else {
+		        arr.reverse();
+		        false
+		    }
+		}
 	}
 	pub mod geometry {
 		use std::ops::{Add, AddAssign, BitXor, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
